577a449c549cbbc074d70af1a0327b9fe4144c85
Tyler Jenkins <tyler.jenkins@quevera.com>
2020-10-15 09:12:18 -0400

    added list ingredients endpoints

--- app/core/models.py
+++ app/core/models.py
@@ -45,5 +45,13 @@ class Tag(models.Model):
     def __str__(self):
         return self.name
 
+class Ingredient(models.Model):
+    name = models.CharField(max_length=225)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    def __str__(self):
+        return self.name
 
 
